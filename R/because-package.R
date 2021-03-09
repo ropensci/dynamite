7ba486918a166989eea9bcf88b595bcd11a7ce36
helske <jouni.helske@iki.fi>
2021-03-09 11:08:13 +0200

    rstan package skeleton

---
+++ R/because-package.R
@@ -0,0 +1,16 @@
+#' The 'because' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name because-package
+#' @aliases because
+#' @useDynLib because, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
