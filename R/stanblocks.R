6ae77d4822787e82b2769216a697b17101bd44ba
helske <jouni.helske@iki.fi>
2022-01-18 16:25:58 +0200

    fixed stan code blocks

--- R/stanblocks.R
+++ R/stanblocks.R
@@ -34,10 +34,13 @@ create_data <- function(formula, ...) {
         "int<lower=1> T; // number of time points",
         "int<lower=1> N; // number of individuals",
         "int<lower=1> M; // number of hidden states",
-        "int<lower=0, upper=T> T_fixed_start;",
-        "int<lower=0, upper=T> T_fixed_end;",
-        "int<lower=1> C; // number of channels", sep = "\n")
+        "int<lower=0, upper=T> T_fixed_start; // fix to state one",
+        "int<lower=0, upper=T> T_fixed_end; // fix to state M",
+        "int<lower=1> C; // number of channels/response variables", sep = "\n")
 
+    # loop over channels
+    # TODO now using formula$resp and formula$families, should probably have
+    # formula$resp[i]$family, formula$resp[i]$name or something
     for(i in seq_along(formula$resp)) {
 
         if(formula$families[i] %in% continuous_distributions) {
@@ -46,25 +49,28 @@ create_data <- function(formula, ...) {
             type <- "int"
         }
 
+        # create response for channel i as T x N array
         y <- paste0(type, paste0(" response_", i), "[T,N];")
         mtext <- paste(mtext, y, sep = "\n")
+        # covariates related to channel i as array of N x K_i matrices
         mtext <- paste(mtext,
-            paste0("int<lower=0> K", i),
+            paste0("int<lower=0> K_", i),
             paste0("matrix[N, K_", i, "] X_", i, "[T];"),
             sep = "\n")
 
-        # TODO, need to add distribution-specific components as well, e.g, number of symbols
+        # TODO, need to add other distribution-specific components as well
+        if(formula$families[i] == "categorical") {
+            mtext <- paste(mtext,  paste0("int<lower=0> S_", i), sep = "\n")
+        }
     }
-    mtext <- paste("data {", mtext, "}", sep="\n") # no indentation... TODO?
+    mtext <- paste("data {", mtext, "}", sep="\n") # TODO indentation?
     mtext
 }
 
 #'
 #' @export
 create_transformed_data <- function(formula, ...) {
-    #TODO
-    # Transform X by centering and maybe even scaling in order to make it more
-    # reasonable to use common sigma for all coefficients
+    # Consider transforming X by centering and maybe scaling, or do it in R?
     mtext <- ""
     mtext <- paste("transformed data {", mtext, "}", sep="\n")
     mtext
@@ -83,25 +89,32 @@ create_parameters <- function(formula, ...) {
     # Not the most efficient way but gets the job done
     for(i in seq_along(formula$resp)) {
 
-        if(formula$families[i] == "categorical") { # Fix after definining families
-            intercept_term <-
-                paste0("vector[S_", i, " - 1] intercept_raw_", i, "[M]; // last value as 0 for identifiability")
+        # do we want to handle intercept separately or as first beta?
+        if(formula$families[i] == "categorical") {
+            # last column not sampled but fixed to 0 for identifiability
+            # intercept_term <-
+            #    paste0("vector[S_", i, " - 1] intercept_raw_", i, "[M];")
             beta_term <- paste0("matrix[K_", i, ", S_", i, " - 1] beta_raw_", i, "[M];")
+            sigma_term <- paste0("matrix[K_", i, ", S_", i, " - 1] sigma_beta_", i, ";")
         } else {
-            intercept_term <- paste0("real intercept_raw_", i, "[M];")
+            #intercept_term <- paste0("real intercept_raw_", i, "[M];")
             beta_term <- paste0("vector[K_", i, "] beta_raw_", i, "[M];")
+            sigma_term <- paste0("vector[K_", i, "] sigma_beta_", i, ";")
         }
-        # standard deviations of the random walk priors for parameters
-        # here same sigma is used for for all coefficients, probably not optimal if scales differ.
-        # On the other hand, there's likely not much information to estimate these accurately
-        # but maybe some standardizations are needed for X
-        mtext <- paste(mtext, intercept_term, beta_term,
-            paste0("real<lower=0> sigma_intercept_", i, ";"),
-            paste0("real<lower=0> sigma_beta_", i, ";"),
-            sep = "\n")
+        # # standard deviations of the random walk priors for parameters
+        # # here same sigma is used for for all coefficients, probably not optimal if scales differ.
+        # # and some standardizations are needed for X in this case
+        # mtext <- paste(mtext, intercept_term, beta_term,
+        #     paste0("real<lower=0> sigma_intercept_", i, ";"),
+        #     paste0("real<lower=0> sigma_beta_", i, ";"),
+        #     sep = "\n")
 
-        # TODO, need to add distribution-specific components as well, e.g, sigma for gaussian etc
+        # unique sigmas for all
+        mtext <- paste(mtext, beta_term, sigma_term,
+            sep = "\n")
+        # TODO, need to add distribution-specific parameters
     }
+    # Strict left-to-right model
     mtext <- paste(mtext,
         "vector<lower=0,upper=1>[M-1] A; // diagonal of the transition matrix",
         sep = "\n")
@@ -125,69 +138,73 @@ create_transformed_parameters <- function(formula, ...) {
     for(i in seq_along(formula$resp)) {
 
         if(formula$families[i] == "categorical") { # Fix after definining families
-            intercept_term <-
-                paste0("vector[S_", i, "] intercept_", i, "[M]; // last value as 0 for identifiability")
+            # intercept_term <-
+            #    paste0("vector[S_", i, "] intercept_", i, "[M]; // last column as 0 for identifiability")
             beta_term <- paste0("matrix[K_", i, ", S_", i, "] beta_", i, "[M];")
         } else {
-            intercept_term <- paste0("real intercept_", i, "[M];")
+            #intercept_term <- paste0("real intercept_", i, "[M];")
             beta_term <- paste0("vector[K_", i, "] beta_", i, "[M];")
         }
-        # standard deviations of the random walk priors for parameters
-        # here same sigma is used for for all coefficients, probably not optimal if scales differ.
-        # On the other hand, there's likely not much information to estimate these accurately
-        # but maybe some standardizations are needed for X
+
         mtext <- paste(mtext, intercept_term, beta_term,
             sep = "\n")
     }
 
     mtext <- paste(mtext,
-        "vector<lower=0,upper=1>[M-1] A; // diagonal of the transition matrix",
         "// transition matrix for forward algorithm",
         "matrix[M, M] log_A = inf_mat;",
         "for(i in 1:(M-1)) {",
         "  log_A[i, i] = log(A[i]);",
-        "  log_A[i, i+1] = log(1 - A[i]);",
+        "  log_A[i, i + 1] = log(1 - A[i]);",
         "}",
         "log_A[M, M] = 0;",
         sep = "\n")
 
     # random walks
     for(i in seq_along(formula$resp)) {
-        if(formula$families[i] == "categorical") { # Fix after definining families
+        if(formula$families[i] == "categorical") {
             # N(0, 1) prior for first state, should be user-defined
             rw <- paste(
-                paste0("intercept_", i, "[1] = append_row(intercept_raw_", i, "[1], 0);"),
+                # paste0("intercept_", i, "[1] = append_row(intercept_raw_", i, "[1], 0);"),
+                # TODO define zeros_K_i in transformed data?
                 paste0("beta_", i, "[1] = append_col(beta_raw_", i, "[1], zeros_K_", i,");"),
                 "for(m in 2:M) {",
-                paste0("  intercept_", i, "[m] = intercept_", i, "[m-1] + ",
-                    "sigma_intercept_", i, " * append_row(intercept_raw_", i, "[m], 0);"),
-                paste0("  beta_", i, "[m] = beta_", i, "[m-1] + sigma_beta_", i,
-                    " * append_col(beta_raw_", i, "[m], zeros_K_", i, ");"),
+                #paste0("  intercept_", i, "[m] = intercept_", i, "[m-1] + ",
+                #    "sigma_intercept_", i, " .* append_row(intercept_raw_", i, "[m], 0);"),
+                paste0("  beta_", i, "[m] = beta_", i, "[m - 1] + sigma_beta_", i,
+                    " .* append_col(beta_raw_", i, "[m], zeros_K_", i, ");"),
                 "}",
                 sep = "\n")
-
         } else {
 
             rw <- paste(
-                paste0("intercept_", i, "[1] = intercept_raw_", i, "[1];"),
+                #paste0("intercept_", i, "[1] = intercept_raw_", i, "[1];"),
                 paste0("beta_", i, "[1] = beta_raw_", i, "[1]; "),
                 "for(m in 2:M) {",
-                paste0("  intercept_", i, "[m] = intercept_", i, "[m-1] + sigma_intercept_", i, " * intercept_raw_", i, "[m];"),
-                paste0("  beta_", i, "[m] = beta_", i, "[m-1] + sigma_beta_", i, " * beta_raw_", i, "[m];"),
+                #paste0("  intercept_", i, "[m] = intercept_", i, "[m-1] + sigma_intercept_", i, " .* intercept_raw_", i, "[m];"),
+                paste0("  beta_", i, "[m] = beta_", i, "[m-1] + sigma_beta_", i, " .* beta_raw_", i, "[m];"),
                 "}",
                 sep = "\n")
         }
         mtext <- paste(mtext, rw, sep = "\n")
     }
     # likelihood terms
+    distributions <- character(length(formula$resp))
     for(i in seq_along(formula$resp)) {
-        # TODO
-        # get_stan_distribution(formula$families[i], i)
-        distribution <- "normal_pdf(response_i[t] | intercept_i[m] + X_i[t] * beta_i[m], sigma_i)"
+        # TODO get_stan_distribution(formula$families[i], i)?
+
+        if(formula$families[i] == "categorical") {
+           distributions[i] <-
+               # all individuals at once
+               "categorical_logit_glm_lupmf(response_i[t] | X_i[t], zeros_S_i, beta_i[m])"
+        } else {
+            #distribution <- "normal_lpdf(response_i[t] | intercept_i[m] + X_i[t] * beta_i[m], sigma_i)"
+            stop("Only categorical sequences are currently supported.")
+            }
         ll <- paste(
             "for(t in 1:T) {",
             "  for(m in 1:M) {",
-            paste0("    log_py[m, t] += ", distribution, ";"),
+            "    log_py[m, t] += ", paste0(distributions, collapse = " +\n"), ";",
             "  }",
             "}",
             sep = "\n")
@@ -208,7 +225,7 @@ create_transformed_parameters <- function(formula, ...) {
 #'
 #' @export
 create_model <- function(formula, ...) {
-    #TODO
+    #TODO user-defined priors
 
     # Prior for staying in same state
     # default prior could be something like this:
@@ -216,10 +233,9 @@ create_model <- function(formula, ...) {
 
     # Some (default) priors for random walk standard deviations
     mtext <- paste(mtext,
-        "sigma_beta_1 ~ std_normal();",
-    "sigma_beta_2 ~ std_normal();",
-    "sigma_intercept_1 ~ std_normal();",
-    "sigma_intercept_2 ~ std_normal();",
+        "for(m in 1:M) {",
+        paste0("  sigma_beta_", 1:length(formula$resp), " ~ std_normal();", collapse = "\n"),
+        "}",
         sep = "\n")
 
     # random walks
@@ -228,22 +244,22 @@ create_model <- function(formula, ...) {
         if(formula$families[i] == "categorical") {
             rw <- paste(
                 paste0("to_vector(beta_raw_", i, "[m]) ~ std_normal();"),
-                paste0("intercept_raw_", i, "[m] ~ std_normal();"),
+               # paste0("intercept_raw_", i, "[m] ~ std_normal();"),
                 sep = "\n")
 
         } else {
             rw <- paste(
                 paste0("beta_raw_", i, "[m] ~ std_normal();"),
-                paste0("intercept_raw_", i, "[m] ~ std_normal();"),
+               # paste0("intercept_raw_", i, "[m] ~ std_normal();"),
                 sep = "\n")
         }
         mtext <- paste(mtext, rw, sep = "\n")
     }
 
 
-    # defined in the functions block (see likelihoods.stan)
+    # defined in the functions block (see loglik_stan.R)
     mtext <- paste(mtext,
-        "target += loglik_uc_safe(log_py, M, T, log_A);",
+        "target += loglik(log_py, M, T, log_A);",
         sep = "\n")
 
     mtext <- paste("model {", mtext, "}", sep="\n")
@@ -253,7 +269,7 @@ create_model <- function(formula, ...) {
 #'
 #' @export
 create_generated_quantities <- function(formula, ...) {
-    #TODO
+    #TODO?
     mtext <- ""
     mtext <- paste("generated quantities {", mtext, "\n}")
     mtext
