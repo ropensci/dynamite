03b4accfe7ee3b80625c630dda821724f9d415b5
Santtu Tikka <santtu.tikka@jyu.fi>
2022-02-07 14:10:28 +0200

    categorical model compiles, data input still wip

--- R/stanblocks.R
+++ R/stanblocks.R
@@ -31,13 +31,13 @@ create_data <- function(formula, ...) {
 
     # Maybe not the best way...
     mtext <- paste(
-        "int<lower=1> T; // number of time points",
-        "int<lower=1> N; // number of individuals",
-        "int<lower=1> C; // number of channels/response variables",
-        "int<lower=1> K; // total number of covariates across all channels",
-        "matrix[N, K] X[T]; // all covariates as an array of N x K matrices",
-        "int<lower=0> D; // number of B-splines",
-        "matrix[D, T] Bs; // B-spline basis matrix",
+        "    int<lower=1> T; // number of time points",
+        "    int<lower=1> N; // number of individuals",
+        "    int<lower=1> C; // number of channels/response variables",
+        "    int<lower=1> K; // total number of covariates across all channels",
+        "    matrix[N, K] X[T]; // all covariates as an array of N x K matrices",
+        "    int<lower=0> D; // number of B-splines",
+        "    matrix[D, T] Bs; // B-spline basis matrix",
         sep = "\n")
 
     # loop over channels
@@ -52,19 +52,19 @@ create_data <- function(formula, ...) {
         }
 
         # create response for channel i as T x N array
-        y <- paste0(type, paste0(" response_", i), "[T,N];")
+        y <- paste0("    ", type, paste0(" response_", i), "[T,N];")
         mtext <- paste(mtext, y, sep = "\n")
         # Number of covariates in channel i
-        mtext <- paste(mtext, paste0("int<lower=1> K_", i, ";"))
+        mtext <- paste(mtext, paste0("    int<lower=1> K_", i, ";"), sep = "\n")
         # index vector of covariates related to channel i
-        mtext <- paste(mtext, paste0("int J_", i, "[K_", i, "];"), sep = "\n")
+        mtext <- paste(mtext, paste0("    int J_", i, "[K_", i, "];"), sep = "\n")
 
         # TODO, need to add other distribution-specific components as well
         if (formula$families[i] == "categorical") {
-            mtext <- paste(mtext,  paste0("int<lower=0> S_", i, ";"), sep = "\n")
+            mtext <- paste(mtext,  paste0("    int<lower=0> S_", i, ";"), sep = "\n")
         }
     }
-    mtext <- paste("data {", mtext, "}", sep="\n") # TODO indentation?
+    mtext <- paste("data {", mtext, "}", sep = "\n")
     mtext
 }
 
@@ -73,10 +73,16 @@ create_data <- function(formula, ...) {
 create_transformed_data <- function(formula, ...) {
     # TODO: Consider transforming X by centering, typically improves sampling efficiency
     # Should center over all time points?
-    # TODO: Add vectors of zeros e.g. row_vector[K] zeros_K = rep_row_vector(0, K);
-    # At least for categorical sequences
-    mtext <- ""
-    mtext <- paste("transformed data {", mtext, "}", sep="\n")
+    transformed_data <- character(0)
+    for (i in seq_along(formula$resp)) {
+        if (formula$families[i] == "categorical") {
+            zeros_term <- paste0("    vector[K_", i, "] zeros_K_", i, " = rep_vector(0, K_", i, ");",
+                                 "    vector[S_", i, "] zeros_S_", i, " = rep_vector(0, S_", i, ");")
+            c(transformed_data) <- zeros_term
+        }
+    }
+    transformed_data <- paste0(transformed_data, collapse = "\n")
+    mtext <- paste("transformed data {", transformed_data, "}", sep = "\n")
     mtext
 }
 
@@ -85,28 +91,28 @@ create_transformed_data <- function(formula, ...) {
 create_parameters <- function(formula, ...) {
 
     mtext <- paste(
-        "// Spline parameters",
-        "// use noncentered parameterisation for sampling efficiency", sep = "\n")
+        "    // Spline parameters",
+        "    // use noncentered parameterisation for sampling efficiency", sep = "\n")
 
     # Not the most efficient way but gets the job done
     for (i in seq_along(formula$resp)) {
 
         # do we want to handle intercept separately or as first beta?
         if (formula$families[i] == "categorical") {
-            a_raw_term <- paste0("row_vector[D] a_raw_", i, "[S_", i, "- 1, K_", i, "];")
+            a_raw_term <- paste0("    row_vector[D] a_raw_", i, "[S_", i, "- 1, K_", i, "];")
             # Do we want separate tau for K coefficients or K * (S - 1) coefficients?
-            tau_term <- paste0("vector<lower=0>[K_", i, "] tau_", i, ";")
+            tau_term <- paste0("    vector<lower=0>[K_", i, "] tau_", i, ";")
         } else {
             # TODO: need to add distribution-specific parameters i.e sigma for gaussian case
-            a_raw_term <- paste0("row_vector[D] a_raw_", i, "[K_", i, "];")
-            tau_term <- paste0("vector<lower=0>[K_", i, "] tau_", i, ";")
+            a_raw_term <- paste0("    row_vector[D] a_raw_", i, "[K_", i, "];")
+            tau_term <- paste0("    vector<lower=0>[K_", i, "] tau_", i, ";")
         }
         mtext <- paste(mtext, a_raw_term, tau_term, sep = "\n")
     }
     if (formula$splines$shrinkage) {
-        mtext <- paste(mtext, "vector<lower=0>[D - 1] lambda; // shrinkage parameter", sep = "\n")
+        mtext <- paste(mtext, "    vector<lower=0>[D - 1] lambda; // shrinkage parameter", sep = "\n")
     }
-    mtext <- paste("parameters {", mtext, "}", sep = "\n") # no indentation... TODO?
+    mtext <- paste("parameters {", mtext, "}", sep = "\n")
     mtext
 }
 
@@ -114,51 +120,52 @@ create_parameters <- function(formula, ...) {
 #' @export
 create_transformed_parameters <- function(formula, ...) {
 
-    mtext <- NULL
     # define variables
+    beta_terms <- character(0)
+    a_terms <- character(0)
     for (i in seq_along(formula$resp)) {
         if (formula$families[i] == "categorical") { # Fix after definining families
-            beta_term <- paste0("matrix[K_", i, ", S_", i, "] beta_", i, "[T];")
-            a_term <- paste0("row_vector[D] a_", i, "[S_", i, " - 1, K_", i, "];")
+            c(beta_terms) <- paste0("    matrix[K_", i, ", S_", i, "] beta_", i, "[T];")
+            c(a_terms) <- paste0("    row_vector[D] a_", i, "[S_", i, " - 1, K_", i, "];")
         } else {
-            beta_term <- paste0("vector[K_", i, "] beta_", i, "[T];")
-            a_term <- paste0("row_vector[D] a_", i, "[K_", i, "];")
+            c(beta_terms) <- paste0("    vector[K_", i, "] beta_", i, "[T];")
+            c(a_terms) <- paste0("    row_vector[D] a_", i, "[K_", i, "];")
         }
-
-        mtext <- paste(mtext, beta_term, a_term, sep = "\n")
     }
+    beta_terms <- paste0(beta_terms, collapse = "\n")
+    a_terms <- paste0(a_terms, collapse = "\n")
+    mtext <- paste(beta_terms, a_terms, sep = "\n")
 
     for (i in seq_along(formula$resp)) {
         if (formula$families[i] == "categorical") {
             # N(0, 1) prior for first a, should be user-defined
             spline_term <- paste(
                 # TODO define zeros_K_i in transformed data?
-                paste0("for(s in 1:(S_", i, " - 1)) {"),
-                paste0("  for(k in 1:K_", i, ") {"),
-                paste0("    a_", i, "[s, k, 1] = a_raw_", i, "[s, k, 1];"),
-                "    for (i in 2:D) {",
-                paste0("      a_", i, "[s, k, i] = a_", i, "[s, k, i-1] + a_raw_", i, "[s, k, i] * tau_", i, "[k] * lambda[i - 1];"),
-                "    }",
-                "    for(t in 1:T){",
-                paste0("      beta_", i, "[t, k, s] = a_", i, "[s, k] * Bs[, t];"),
-                "    }",
-                "  }",
-                "}",
-                "for(t in 1:T){",
-                paste0("  beta_", i, "[t, , S_", i, "] = zeros_K_", i, ";"),
-                "}", sep = "\n")
+                paste0("    for (s in 1:(S_", i, " - 1)) {"),
+                paste0("        for (k in 1:K_", i, ") {"),
+                paste0("            a_", i, "[s, k, 1] = a_raw_", i, "[s, k, 1];"),
+                       "            for (i in 2:D) {",
+                paste0("                a_", i, "[s, k, i] = a_", i, "[s, k, i-1] + a_raw_", i, "[s, k, i] * tau_", i, "[k] * lambda[i - 1];"),
+                       "            }",
+                       "            for (t in 1:T) {",
+                paste0("                beta_", i, "[t, k, s] = a_", i, "[s, k] * Bs[, t];"),
+                       "            }",
+                       "        }",
+                       "    }",
+                       "    for (t in 1:T) {",
+                paste0("        beta_", i, "[t, ,S_", i, "] = zeros_K_", i, ";"),
+                       "    }", sep = "\n")
         } else {
-
             spline_term <- paste(
-                paste0("for(k in 1:K_", i, ") {"),
-                paste0("  a_", i, "[k, 1] = a_raw_", i, "[k, 1];"),
-                "  for (i in 2:D) {",
-                paste0("    a_", i, "[k, i] = a_", i, "[k, i-1] + a_raw_", i, "[k, i] * tau_", i, "[k] * lambda[i - 1];"),
-                "  }",
-                "  for(t in 1:T){",
-                paste0("    beta_", i, "[t, k] = a_", i, "[k] * Bs[, t];"),
-                "  }",
-                "}", sep = "\n")
+                paste0("    for (k in 1:K_", i, ") {"),
+                paste0("        a_", i, "[k, 1] = a_raw_", i, "[k, 1];"),
+                       "        for (i in 2:D) {",
+                paste0("            a_", i, "[k, i] = a_", i, "[k, i-1] + a_raw_", i, "[k, i] * tau_", i, "[k] * lambda[i - 1];"),
+                       "        }",
+                       "        for (t in 1:T) {",
+                paste0("            beta_", i, "[t, k] = a_", i, "[k] * Bs[, t];"),
+                       "        }",
+                       "    }", sep = "\n")
         }
         mtext <- paste(mtext, spline_term, sep = "\n")
     }
@@ -166,40 +173,42 @@ create_transformed_parameters <- function(formula, ...) {
     mtext
 }
 
-
 #'
 #' @export
 create_model <- function(formula, ...) {
     # TODO: user-defined priors
     #
-    mtext <- if (formula$splines$shrinkage) {
-        "lambda ~ std_normal();  // prior for shrinkage terms"
-    } else NULL
-    mtext <- paste(mtext, paste0("tau_", 1:length(formula$families), " ~ cauchy(0, 1);", collapse = "\n"), sep = "\n")
+    priors <- character(0)
+    if (formula$splines$shrinkage) {
+        c(priors) <- "    lambda ~ std_normal();  // prior for shrinkage terms"
+    }
+    c(priors) <- paste0("    tau_", 1:length(formula$families), " ~ cauchy(0, 1);")
+    mtext <- paste0(priors, collapse = "\n")
     for (i in seq_along(formula$families)) {
         if (formula$families[i] == "categorical") {
             a_term <- paste(
-                paste0("for(s in 1:(S_", i, " - 1)) {"),
-                paste0("  for(k in 1:K_", i, ") {"),
-                paste0("    a_raw_", i, "[s, k] ~ std_normal();"),
-                "  }",
-                "}", sep = "\n")
+                paste0("    for (s in 1:(S_", i, " - 1)) {"),
+                paste0("        for (k in 1:K_", i, ") {"),
+                paste0("            a_raw_", i, "[s, k] ~ std_normal();"),
+                       "        }",
+                       "    }", sep = "\n")
         } else {
             a_term <- paste(
-                paste0("for(k in 1:K_", i, ") {"),
-                paste0("  a_raw_", i, "[k] ~ std_normal();"),
-                "}", sep = "\n")
+                paste0("    for (k in 1:K_", i, ") {"),
+                paste0("        a_raw_", i, "[k] ~ std_normal();"),
+                       "    }", sep = "\n")
         }
         mtext <- paste(mtext, a_term, sep = "\n")
     }
     for (i in seq_along(formula$families)) {
         if (formula$families[i] == "categorical") {
             likelihood_term <-
-                paste0("  target += categorical_logit_glm_lupmf(response_", i, "[t] | X[t][, J_",i , "], zeros_S_", i, ", beta_", i, "[t]);")
+                paste0("        target += categorical_logit_glm_lupmf(response_", i,
+                       "[t] | X[t][,J_", i, "], zeros_S_", i, ", beta_", i, "[t]);")
         } else {
             stop("Likelihoods not yet defined for non-categorical data")
         }
-        mtext <- paste(mtext, "for(t in 1:T) {", likelihood_term, "}", sep = "\n")
+        mtext <- paste(mtext, "    for (t in 1:T) {", likelihood_term, "    }", sep = "\n")
     }
 
     mtext <- paste("model {", mtext, "}", sep = "\n")
