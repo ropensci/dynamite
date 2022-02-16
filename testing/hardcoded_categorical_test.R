459fb65757510d50d3e52b0450231d9cc7ac7b37
helske <jouni.helske@iki.fi>
2022-02-16 11:07:48 +0200

    separate intercept for splines

--- testing/hardcoded_categorical_test.R
+++ testing/hardcoded_categorical_test.R
@@ -14,7 +14,7 @@ softmax <- function (x) {
 create_data <- function(T, N, S, K, n, tau = runif(K)) {
 
     X <- array(0, c(T, N, K))
-    X[] <- rnorm(length(X))
+    X[] <- rnorm(length(X), 1)
 
     knots <- seq(1, T, length.out = n + 2)[-c(1, n + 2)]
     Bsplines <- t(splines::bs(1:T, knots = knots, degree = 3, intercept = TRUE))
@@ -44,7 +44,7 @@ create_data <- function(T, N, S, K, n, tau = runif(K)) {
     }
 
     TraMineR::seqdplot(TraMineR::seqdef(t(y)))
-    list(N = N, T = T, y = y, X = X,  K = K, S = S, B = Bsplines, D = D,
+    list(N = N, T = T, y = y, X = X,  K = K, S = S, B = Bsplines, D = D, knots = knots,
         tau = tau, beta = beta, a = a)
 }
 
@@ -167,4 +167,106 @@ print(fit_nc, pars = "tau")
 ess_c <- summary(fit_c, pars = c("tau", "beta"))[[1]][, "n_eff"]
 ess_nc <- summary(fit_nc, pars = c("tau", "beta"))[[1]][, "n_eff"]
 summary(ess_c / ess_nc)
+
+
+
+
+## Without intercept use sum-to-zero-constraint
+
+
+create_data0 <- function(T, N, S, K, n, tau = runif(K)) {
+
+    X <- array(0, c(T, N, K))
+    X[] <- rnorm(length(X))
+
+    knots <- seq(1, T, length.out = n + 2)[-c(1, n + 2)]
+    B <- splines::bs(1:T, knots = knots, degree = 3, intercept = FALSE)
+    # sum-to-zero constraint
+    Z <- qr.Q(qr(colSums(B)), complete = TRUE)[, 2:ncol(B)]
+    Bsplines <- t(B %*% Z)
+    D <- nrow(Bsplines)
+
+    #matplot(t(Bsplines),type="l")
+
+    a_raw <- array(0, c(S-1, K, D))
+    a_raw[] <- rnorm(length(a_raw))
+    beta <- array(0, c(T, S, K))
+    beta0 <- cbind(matrix(rnorm(K * (S - 1)), S - 1, K), 0) # intercept for beta
+    a <- array(0, c(S-1, K, D))
+    for(s in 1:(S-1)) {
+        for(k in 1:K) {
+            a[s, k, 1] <- a_raw[s, k, 1]
+            for (i in 2:D) {
+                a[s, k, i] = a[s, k, i-1] + a_raw[s, k, i] * tau[k]
+            }
+            for(t in 1:T){
+                beta[t, s, k] = beta0[s, k] + a[s, k, ] %*% Bsplines[, t]
+            }
+        }
+    }
+    y <- matrix(NA, T, N)
+    for(t in 1:T) {
+        for(i in 1:N){
+            y[t, i] <- sample(1:S, size = 1, prob = softmax(X[t, i, ] %*% t(beta[t, , ])))
+        }
+    }
+
+    TraMineR::seqdplot(TraMineR::seqdef(t(y)))
+    list(N = N, T = T, y = y, X = X,  K = K, S = S, B = Bsplines, D = D, knots = knots,
+        tau = tau, beta0 = beta0, beta = beta, a = a)
+}
+
+set.seed(1)
+d <- create_data0(T = 50, N = 200, S = 3, K = 1, n = 10, tau = 0.1)
+
+
+model_beta0 <- stan_model("hardcoded_categorical_case_centered_intercept.stan")
+
+fit_beta0 <- sampling(model_beta0, data = d,
+    chains = 1, cores = 1,
+    refresh = 100, iter= 2000,
+    seed = 1)
+
+print(fit_beta0, "tau")
+d$tau
+
+print(fit_beta0, "beta0")
+d$beta0
+
+b <- extract(fit_beta0, "beta")[[1]]
+mean(b[,,1,1])
+mean(b[,,2,1])
+
+
+## generate data as before but fit sum-to-zero version
+set.seed(234)
+d <- create_data(T = 100, N = 200, S = 3, K = 1, n = 10, tau = 0.5)
+
+fit_c <- sampling(model_centered, data = d,
+    chains = 1, cores = 1,
+    refresh = 500, iter= 2000,
+    seed = 1)
+
+d2 <- d
+# sum-to-zero constraint
+Z <- qr.Q(qr(colSums(d$B)), complete = TRUE)[, 2:ncol(d$B)]
+d2$B <- t(d$B %*% Z)
+d2$D <- nrow(d2$B)
+fit_beta0 <- sampling(model_beta0, data = d2,
+    chains = 1, cores = 1,
+    refresh = 500, iter= 2000,
+    seed = 1)
+
+print(fit_c,"tau")
+print(fit_beta0,"tau")
+
+b <- apply(extract(fit_c, "beta")[[1]], 2:3, mean)
+b0 <- apply(extract(fit_beta0, "beta")[[1]], 2:3, mean)
+
+ts.plot(cbind(b[,1:2], b0[,1:2], d$beta[, 1:2, 1]), col = rep(1:3, each = 2), lty = 1:2)
+
+print(fit_beta0,"beta0")
+colMeans(d$beta)
+colMeans(b0)
+colMeans(b)
 
