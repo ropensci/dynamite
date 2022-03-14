4c21856b1a17e7abb23ff711f2273fab4bd32c56
helske <jouni.helske@iki.fi>
2022-03-14 11:56:37 +0200

    small print method

---
+++ R/print.R
@@ -0,0 +1,18 @@
+#' Print Summary of btvcm Object
+#'
+#' Prints the summary information of time-invariant model parameters.
+#'
+#' @param x An output from \code{\link{btvcm}}.
+#' @param pars A character vector of parameter names.
+#' The default is to print summary of spline coefficient standard deviations tau.
+#' @param ... Additional arguments to \code{\link{print.stanfit}}.
+#' @method print btvcmfit
+#' @export
+print.btvcmfit <- function(x, pars, ...) {
+    if (missing(pars)) {
+        all_pars <- x$stanfit@sim$pars_oi
+        idx <-  c(grep("^tau", all_pars), grep("lambda",all_pars))
+        pars <- all_pars[idx]
+    }
+    print(x$stanfit, pars = pars, ...)
+}
