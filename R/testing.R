78c4cada477e353a461d2f3582eeffaec965358b
Santtu Tikka <santtu.tikka@jyu.fi>
2022-03-23 17:36:51 +0200

    offset for Poisson, convert character data columns to factors

--- R/testing.R
+++ R/testing.R
@@ -11,6 +11,8 @@ test_data <- data.frame(y1 = as.factor(sample(2, size = TN, replace = TRUE)),
     x2 = as.factor(sample(4, size = TN, replace = TRUE)),
     x3 = rnorm(TN),
     x4 = rnorm(TN),
+    x5 = sample(letters[1:4], size = TN, replace = TRUE),
+    t = sample(50:100, size = TN, replace = TRUE),
     ID = gl(N, T),
     time = 1:T)
 
@@ -25,6 +27,10 @@ test_form2 <- obs(y1 ~ x1 + x2 + x4 + lag(y1, 1) + lag(y2, 1) + lag(y3, 1), fami
     obs(y2 ~ x1 + x3 + x4 + lag(y1, 1) + lag(y2, 1) + lag(y3, 1), family = categorical()) +
     obs(y3 ~ x1 + x3 + lag(y1, 1) + lag(y2, 1) + lag(y3, 1), family = categorical()) +
     splines()
+
+test_offset <- obs(y1 ~ x1 + x2 + offset(log(t)), family = poisson()) + splines()
+#test_fit <- btvcm:::btvcmfit(test_offset, test_data, ID, t, debug = list(no_compile = TRUE, model_matrix = TRUE, model_data = TRUE, model_code = TRUE))
+#test_fit <- btvcm:::btvcmfit(test_offset, test_data, ID, t)
 
 # Use these to control what is returned, wrap in argument debug:
 #     no_compile = TRUE => stan model is not compiled, implies no_sampling = TRUE
