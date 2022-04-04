e3ea256e82b8f5802300f98c6ec359ac071d414a
Santtu Tikka <santtu.tikka@jyu.fi>
2022-04-04 18:16:13 +0300

    combine beta_fixed and beta_varying for output

--- R/testing.R
+++ R/testing.R
@@ -10,6 +10,7 @@ test_data <- data.frame(
     y3 = as.factor(sample(5, size = TN, replace = TRUE)),
     y4 = sample(5, size = TN, replace = TRUE),
     y5 = rnorm(n = TN, mean = 1, sd = 2),
+    y6 = rpois(n = TN, lambda = 0.5),
     x1 = rnorm(TN),
     x2 = as.factor(sample(4, size = TN, replace = TRUE)),
     x3 = rnorm(TN),
@@ -33,8 +34,8 @@ test_form2 <- obs(y1 ~ x1 + x2 + x4 + lag(y1, 1) + lag(y2, 1) + lag(y3, 1), fami
     obs(y3 ~ x1 + x3 + lag(y1, 1) + lag(y2, 1) + lag(y3, 1), family = categorical()) +
     splines()
 
-test_all <- obs(y4 ~ -1 + x1 + varying(~x2) + offset(log(t)), family = poisson()) +
-    obs(y2 ~ -1 + x1 + varying(~x3) + trials(n), family = binomial()) +
+test_all <- obs(y6 ~ -1 + x1 + varying(~x2) + offset(log(t)), family = poisson()) +
+    obs(y4 ~ -1 + x1 + varying(~x3) + trials(n), family = binomial()) +
     obs(y5 ~ x4 + fixed(~x5) + varying(~-1 + lag(y5)), family = gaussian()) +
     obs(y1 ~ x1 + x2 + x4 + lag(y1, 1) + lag(y2, 1), family = categorical()) +
     splines()
