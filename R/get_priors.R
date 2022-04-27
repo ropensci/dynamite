63e1aed18877d1a777522f0fec31eeb02ba9f40c
helske <jouni.helske@iki.fi>
2022-04-27 10:35:21 +0300

    fix group/time NSE issue

--- R/get_priors.R
+++ R/get_priors.R
@@ -1,23 +1,31 @@
 #' @export
 get_priors <- function(formula, data, group, time) {
-
-    if (!is.character(group)) {
-        group <- deparse(substitute(group))
-    }
-    if (!is.character(time)) {
-        time <- deparse(substitute(time))
-    }
-    btvcmfit(formula, data, group, time, debug = list(no_compile = TRUE))$priors
+    out <- do.call("btvcmfit",
+        list(formula = formula,
+            data = data,
+            group = substitute(group),
+            time = substitute(time),
+            debug = list(no_compile = TRUE)))
+    out$priors
 }
 #' @export
 get_code <- function(formula, data, group, time) {
-    if (!is.character(group)) {
-        group <- deparse(substitute(group))
-    }
-    if (!is.character(time)) {
-        time <- deparse(substitute(time))
-    }
-    btvcmfit(formula, data, group, time, debug = list(no_compile = TRUE, model_code = TRUE))$model_code
+    out <- do.call("btvcmfit",
+        list(formula = formula,
+            data = data,
+            group = substitute(group),
+            time = substitute(time),
+            debug = list(no_compile = TRUE, model_code = TRUE)))
+    out$model_code
 }
-
+#' @export
+get_data <- function(formula, data, group, time) {
+    out <- do.call("btvcmfit",
+        list(formula = formula,
+            data = data,
+            group = substitute(group),
+            time = substitute(time),
+            debug = list(no_compile = TRUE, model_data = TRUE)))
+    out$model_data
+}
 
