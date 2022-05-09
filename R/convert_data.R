1ad0c03e4e401469b2c853ced1be30f1ae447136
helske <jouni.helske@iki.fi>
2022-05-09 11:47:17 +0300

    fix typo

--- R/convert_data.R
+++ R/convert_data.R
@@ -332,7 +332,7 @@ prepare_channel_categorical <- function(y, Y, channel,
         response = y,
         prior = paste0("normal(", m, ", ", s, ")"),
         type = "delta",
-        category = lrep(levels_, each = S_y - 1)
+        category = rep(levels_, each = S_y - 1)
       )
       channel$tau_prior_npars <- 2
       channel$tau_prior_pars <- cbind(0, rep(1, channel$K_varying))
