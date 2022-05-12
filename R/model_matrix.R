e14e14dcf58cd05c847a6bfb73e6a35aa65ec538
helske <jouni.helske@iki.fi>
2022-05-12 15:29:10 +0300

    get_resp to get_responses

--- R/model_matrix.R
+++ R/model_matrix.R
@@ -11,7 +11,7 @@ full_model.matrix <- function(dformula, data) {
   u_names <- unique(colnames(model_matrix))
   model_matrix <- model_matrix[, u_names, drop = FALSE]
   n_models <- length(model_matrices)
-  y_names <- get_resp(dformula)
+  y_names <- get_responses(dformula)
   empty_list <- setNames(vector(mode = "list", length = n_models), y_names)
   attr(model_matrix, "assign") <- empty_list
   attr(model_matrix, "fixed") <- empty_list
