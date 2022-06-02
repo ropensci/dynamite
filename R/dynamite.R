56a76edf693770fd3ba5271ac5d6a4b55abfb5f8
Santtu Tikka <santtu.tikka@jyu.fi>
2022-06-02 10:57:38 +0300

    as.data.table namespace

--- R/dynamite.R
+++ R/dynamite.R
@@ -132,7 +132,7 @@ parse_data <- function(data, group_var, time_var) {
         dplyr::left_join(data, by = c(group_var, time_var))
     }
   }
-  data <- as.data.table(data)
+  data <- data.table::as.data.table(data)
   data.table::setkeyv(data, c(group_var, time_var))
   data
 }
