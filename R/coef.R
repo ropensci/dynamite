bc1a8840679185149eb76b61195a649b71893dd3
helske <jouni.helske@iki.fi>
2022-06-14 10:02:15 +0300

    extract nu with coef as well

--- R/coef.R
+++ R/coef.R
@@ -5,7 +5,8 @@
 #' @export
 #' @param object An object of class \code{dynamitefit}.
 #' @param type  \[`character(1)`]\cr Either `beta` (the default) for
-#'   time-invariant coefficients,  or `delta` for time-varying coefficients.
+#'   time-invariant coefficients,  `delta` for time-varying coefficients, or
+#'   `nu` for random intercepts.
 #' @param include_alpha \[`logical(1)`]\cr If `TRUE` (default), extracts also
 #'   time-invariant intercept term alpha if time-invariant parameters beta are
 #'   extracted, and time-varying alpha if time-varying delta are extracted.
@@ -17,11 +18,11 @@
 #'
 #' @srrstats {G2.3a} *Use `match.arg()` or equivalent where applicable to only permit expected values.*
 #' @srrstats {RE4.2} *Model coefficients (via `coeff()` / `coefficients()`)*
-coef.dynamitefit <- function(object, type = c("beta", "delta"),
+coef.dynamitefit <- function(object, type = c("beta", "delta", "nu"),
                              summary = TRUE, probs = c(0.05, 0.95),
                              include_alpha = TRUE, ...) {
   type <- match.arg(type)
-  if (include_alpha) {
+  if (include_alpha || type != "nu") {
     types <- c("alpha", type)
     out <- as.data.frame(object, types = types, summary = summary,
                          probs = probs)
