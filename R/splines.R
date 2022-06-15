d164e555817b02754ed0786bf88e7f86d80b3f04
helske <jouni.helske@iki.fi>
2022-06-15 11:26:02 +0300

    modify spline arguments

--- R/splines.R
+++ R/splines.R
@@ -15,33 +15,33 @@
 #' @param override \[`logical(1)`]\cr If `FALSE` (the default), an existing
 #'    definition for the splines will not be overridden by another call to
 #'    `splines()`. If `TRUE`, any existing definitions will be replaced.
-#' @param lb_tau \[`numeric(1)`]\cr Hard constraint on the lower bound of the
+#' @param lb_tau \[`numeric()`]\cr Hard constraint(s) on the lower bound of the
 #'   standard deviation parameters \eqn{\tau} of the random walk priors. Can be
 #'   useful in avoiding divergences in some cases. See also `noncentered`
-#'   argument.
+#'   argument. Can be a single positive value, or vector defining the
+#'   lower bound separately for each channel.
 #' @param noncentered  \[`logical()`]\cr If `TRUE`, use noncentered
 #'   parameterization for the spline coefficients. Default is `FALSE`. Try
 #'   changing this if you encounter divergences or other problems in sampling.
 #'   Can be a single logical value, or vector of logical values, defining the
 #'   parameterization separately for each channel.
-#' @param df \[`integer(1)`]\cr See [splines::bs()].
-#' @param knots \[`numeric()`]\cr See [splines::bs()].
+#' @param df \[`numeric()`]\cr Degree of freedom, i.e. the total number of
+#'   spline coefficients. See [splines::bs()]. Note that the knots are always
+#'   defined as equidistant sequence on the interval starting from the first
+#'   non-fixed time point to the last time point in the data.
 #' @param degree \[`integer(1)`]\cr See [splines::bs()].
-#' @param Boundary.knots \[`numeric()`]\cr See [splines::bs()].
 #' @export
 #' @srrstats {G2.4} *Provide appropriate mechanisms to convert between different data types, potentially including:*
 #' @srrstats {G2.4a} *explicit conversion to `integer` via `as.integer()`*
 #' @srrstats {G2.4b} *explicit conversion to continuous via `as.numeric()`*
 #'
-# TODO start knots from fixed + 1
-# TODO remove boundary.knots  and knots, and rename df -> k_knots
+#' TODO explain the fixed time points above in more detail (here or
+#' somewhere else)?
 splines <- function(shrinkage = FALSE, override = FALSE,
-                    df = NULL, knots = NULL, degree = 3,
-                    Boundary.knots = NULL, lb_tau = 0, noncentered = FALSE) {
+                    df = NULL, degree = 3, lb_tau = 0, noncentered = FALSE) {
   shrinkage <- try_(shrinkage, type = "logical")[1] # TODO check the length
   override <- try_(override, type = "logical")[1] # TODO
   df <- try_(df, type = "integer")[1]
-  knots <- try_(knots, type = "numeric")
   degree <- try_(degree, type = "integer")
   # length of these this argument can be > 1 for channel-wise definitions,
   # check the length later
@@ -58,12 +58,8 @@ splines <- function(shrinkage = FALSE, override = FALSE,
       noncentered = noncentered,
       bs_opts = list(
         df = df,
-        knots = knots,
         degree = degree,
-        # Always include intercept as we don't have separate intercept for
-        # deltas. Now first spline coefficient corresponds to delta_1
-        intercept = TRUE,
-        Boundary.knots = Boundary.knots
+        intercept = TRUE
       )
     ),
     override = override,
