9c5c7c37efaf7e5c24d201524c8286b73707e2ef
helske <jouni.helske@iki.fi>
2022-06-16 09:57:09 +0300

    reduce the size of data

--- data-raw/gaussian_example_fit.R
+++ data-raw/gaussian_example_fit.R
@@ -6,7 +6,7 @@ gaussian_example_fit <- dynamite(
   obs(y ~ -1 + z + varying(~ x + lag(y)), family = gaussian()) +
   splines(df = 20),
   data = gaussian_example, time = "time", group = "id",
-  iter = 2000, chains = 2, cores = 2, refresh = 0
+  iter = 1000, chains = 2, cores = 2, refresh = 0, save_warmup = FALSE
 )
 
 usethis::use_data(gaussian_example_fit, overwrite = TRUE, compress = "xz")
