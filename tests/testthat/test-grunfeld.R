3f4c2077b5309963cbe9bc5add88128ba1f5ffcf
helske <jouni.helske@iki.fi>
2022-07-08 17:19:38 +0300

    grunfeld test ready

--- tests/testthat/test-grunfeld.R
+++ tests/testthat/test-grunfeld.R
@@ -8,36 +8,45 @@ test_that("parameters of the Grunfield model are recovered", {
   skip_if_not(run_extended_tests)
   library("plm")
 
-
   data(Grunfeld, package = "plm")
   fit_plm <- plm(inv ~ value + capital, data = Grunfeld,
     index = c("firm", "year"), effect = "individual", model = "within")
 
   set.seed(1)
-  # set very vague priors
-  p <- get_priors(obs(inv ~ value + capital,
+  # dynamite defines prior for the intercept based on the mean at the first time
+  # point, which differs from the brms, so use dummy intercept instead in both
+  Grunfeld$intercept <- 1
+  p <- get_priors(obs(inv ~ -1 + intercept + value + capital,
     family = "gaussian", random_intercept = TRUE),
     Grunfeld, "firm", "year")
+  # set very vague priors
   p$prior[] <- rep("normal(0, 1000)", nrow(p))
   fit <- dynamite(obs(inv ~ value + capital,
     family = "gaussian", random_intercept = TRUE),
     Grunfeld, "firm", "year", refresh = 0,
-    chains = 1, cores = 1, iter = 2000)
+    chains = 2, cores = 2, iter = 10000)
 
-  sumr <- summary(fit)
-
-  expect_equal(coef(fit_plm), coef(fit)$mean[3:2],
+  expect_equal(coef(fit_plm), coef(fit)$mean[2:3],
     tolerance = 0.01, ignore_attr = TRUE)
-  library("brms")
-  fit_brm <- brm(inv ~ value + capital + (1 | firm), Grunfeld,
-    prior = prior(normal(0, 1000), class = "b") +
-      prior(normal(0, 1000), class = "Intercept") +
-      prior(normal(0, 1000), class = "sd") +
-      prior(normal(0, 1000), class = "sigma"),
-    refresh = 0,
-    chains = 1, cores = 1, iter = 2000)
-  expect_equal(coef(fit)$mean[c(1,3,2)],
-    fixef(fit_brm)[, "Estimate"], tolerance = 0.1, ignore_attr = TRUE)
-  coef(fit,"nu")$mean-ranef(fit_brm)$firm[, "Estimate", ]
-  #WIP
+
+  # Not run, values are stored
+  # library(brms)
+  # fit_brm <- brm(inv ~ 0 + Intercept + value + capital + (1 | firm),
+  #   Grunfeld,
+  #   prior = prior(normal(0, 1000), class = "b") +
+  #     prior(normal(0, 1000), class = "sd") +
+  #     prior(normal(0, 1000), class = "sigma"),
+  #     refresh = 0, seed = 1,
+  #   chains = 2, cores = 2, iter = 50000, warmup = 5000)
+  # brms_est <- round(unname(posterior_summary(fit_brm)[, "Estimate"]), 6)[1:15]
+  brms_est <- c(-57.837641, 0.109821, 0.308666, 101.016325, 53.070699,
+    -10.084975, 158.146755, -173.565297, 30.013812, -55.072784, 34.483764,
+    -8.106796, 0.644707, -28.183244, 50.348913)
+  sumr <- as_draws(fit) |> posterior::summarise_draws(
+    default_mcse_measures(),
+    default_summary_measures())
+  for(i in 1:15) {
+    expect_equal(sumr$mean[i], brms_est[i],
+      tolerance = 10 * sumr$mcse_mean[i], label = sumr$variable[i])
+  }
 })
